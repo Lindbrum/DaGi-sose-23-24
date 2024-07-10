0ee68a04f72aa46923af87b801647b25d5ac8dcb
Lindbrum <ultrascati95@gmail.com>
2024-07-10 16:55:07 +0200

    Maven archetypes for REST and SOAP services

---
+++ archetypes/RESTServiceArchetype/src/main/resources/archetype-resources/src/main/java/dao/SumTemplateRepository.java
@@ -0,0 +1,9 @@
+package ${package}.dao;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+import ${package}.model.SumTemplate;
+
+public interface SumTemplateRepository extends ListCrudRepository<SumTemplate, Long> {
+
+}
