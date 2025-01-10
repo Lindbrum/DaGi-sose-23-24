b37735681f1d769e819a9105aee3cc5a27b8a4f4
giovannispaziani <giovanni.spaziani@student.univaq.it>
2025-01-10 11:10:05 +0100

    Added code to load balance requests to the two remaining soap clients from the proxy.

--- soap-proxy-prosumer-rest/src/main/java/it/univaq/sose/dagi/soap_proxy_prosumer_rest/client/FeedbackSOAPClient.java
+++ soap-proxy-prosumer-rest/src/main/java/it/univaq/sose/dagi/soap_proxy_prosumer_rest/client/FeedbackSOAPClient.java
@@ -3,9 +3,18 @@ package it.univaq.sose.dagi.soap_proxy_prosumer_rest.client;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 
 import it.univaq.sose.dagi.soap_proxy_prosumer_rest.model.Feedback;
 import it.univaq.sose.dagi.wsdltypes.CreateFeedbackRequest;
@@ -18,45 +27,116 @@ import it.univaq.sose.dagi.wsdltypes.FetchEventFeedbackResponse;
 import it.univaq.sose.dagi.wsdltypes.FetchEventFeedbackResponse.FeedbackList;
 import it.univaq.sose.dagi.wsdltypes.ObjectFactory;
 import it.univaq.sose.dagi.wsdltypes.ServiceException_Exception;
+import jakarta.ws.rs.ServiceUnavailableException;
+import lombok.extern.slf4j.Slf4j;
 
 //This class facilitates communication with a SOAP web service to retrieve feedback data for a specified event.
 //It uses an ObjectFactory to construct SOAP request objects and process responses.
-@Component
+@Slf4j
+@Service
 public class FeedbackSOAPClient {
 
 	private ObjectFactory factory;
-	
-	//@Value("${client.soap.wsdl}")
-		private String wsdlUrl = "http://localhost:8080/api/soap/?wsdl";
-	
-	private EventManagementPort port;
-	
-	public FeedbackSOAPClient() {
-		//super();
+	private URL lastUrl;
+	private EurekaClient eurekaClient;
+	private EventManagementImplService service;
+	private final List<InstanceInfo> lastInstancesCache = Collections.synchronizedList(new ArrayList<>());
+	private static final Logger log = LoggerFactory.getLogger(EventSOAPClient.class);
+
+	private String wsdlUri = "event-management-soap/event-management-soap/?wsdl";
+
+	// It initializes the EventSOAPClient with an ObjectFactory instance.
+	// It sets up the necessary configurations to interact with the SOAP service.
+	public FeedbackSOAPClient(EurekaClient eurekaClient) {
+		// super();
+		this.eurekaClient = eurekaClient;
+		this.service = null;
 		this.factory = new ObjectFactory();
-		URL url;
-		try {
-			url = new URL(wsdlUrl);
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return;
-		}
-		EventManagementImplService service = new EventManagementImplService(url);
-		this.port = service.getEventManagementImplPort();
 	}
 
-	//This method retrieves feedback data for a given event ID. It constructs a SOAP request, sends it to the web service, and
-	//processes the response to extract feedback details. It maps the response data into a list of Feedback objects and
-	//returns this list. If the URL is malformed, it catches and prints any MalformedURLException that occurs.
+	// Returns an instance of the SOAP service among the ones registered to the
+	// discovery server
+	private EventManagementPort getService() {
+		try {
+			List<InstanceInfo> instances = Optional
+					.ofNullable(eurekaClient.getInstancesByVipAddress("event-management-soap", false))
+					.filter(list -> !list.isEmpty()).orElseGet(() -> {
+
+						log.warn("Using cached copy of the event management SOAP service");
+						log.warn("lastInstancesCache {}", lastInstancesCache);
+						synchronized (lastInstancesCache) {
+							return new ArrayList<>(lastInstancesCache); // Return a copy of the cached instances
+						}
+					});
+
+			// Update the instance cache synchronously
+			synchronized (lastInstancesCache) {
+				lastInstancesCache.clear();
+				lastInstancesCache.addAll(deepCopyInstanceInfoList(instances));
+			}
+
+			// Remove the last used instance from the list
+			if (lastUrl != null) {
+				instances.removeIf(instance -> {
+					try {
+						return Objects.equals(new URL(instance.getHomePageUrl() + wsdlUri), lastUrl);
+					} catch (MalformedURLException e) {
+						log.error("Malformed URL while filtering instances: {}", e.getMessage(), e);
+						return false;
+					}
+				});
+			}
+
+			// If no alternative instances are available, use the last used instance
+			if (instances.isEmpty()) {
+				log.warn("No alternative instances available for event-management-soap, using the last used instance");
+				if (service != null) {
+					return service.getEventManagementImplPort();
+				} else {
+					throw new ServiceUnavailableException(
+							"event-management-soap: No alternative instances available and no previously used instance available");
+				}
+			}
+
+			// Shuffle the list to select a random instance
+			Collections.shuffle(instances);
+			InstanceInfo instance = instances.get(0);
+			String eurekaUrl = instance.getHomePageUrl() + wsdlUri;
+			URL url = new URL(eurekaUrl);
+			service = new EventManagementImplService(url);
+			log.info("New Retrieved Event Management SOAP URL: {}", url);
+			lastUrl = url;
+
+			return service.getEventManagementImplPort();
+		} catch (MalformedURLException e) {
+			log.error("Malformed URL: {}", e.getMessage(), e);
+			throw new ServiceUnavailableException("Malformed URL: " + e.getMessage());
+		} catch (Exception e) {
+			log.error("Failed to retrieve Event Management SOAP URL: {}", e.getMessage(), e);
+			throw new ServiceUnavailableException("Failed to retrieve Event Management SOAP URL: " + e.getMessage());
+		}
+	}
+
+	private List<InstanceInfo> deepCopyInstanceInfoList(List<InstanceInfo> instances) {
+		return instances.stream().map(InstanceInfo::new) // Use the copy constructor
+				.collect(Collectors.toList());
+	}
+
+	// This method retrieves feedback data for a given event ID. It constructs a
+	// SOAP request, sends it to the web service, and
+	// processes the response to extract feedback details. It maps the response data
+	// into a list of Feedback objects and
+	// returns this list. If the URL is malformed, it catches and prints any
+	// MalformedURLException that occurs.
 	public List<Feedback> requestEventFeedbacks(Long eventId) throws ServiceException_Exception {
 		FetchEventFeedbackRequest requestParams = factory.createFetchEventFeedbackRequest();
 		requestParams.setEventId(eventId);
-		FetchEventFeedbackResponse response = this.port.fetchEventFeedback(requestParams);
+		EventManagementPort port = getService(); //load balancing
+		FetchEventFeedbackResponse response = port.fetchEventFeedback(requestParams);
 		FeedbackList feedbackList = response.getFeedbackList();
 		List<FeedbackData> xmlFeedbacks = feedbackList.getFeedbackData();
 		List<Feedback> feedbacks = new ArrayList<>();
-		for(FeedbackData xmlFeedback : xmlFeedbacks) {
+		for (FeedbackData xmlFeedback : xmlFeedbacks) {
 			Feedback feedback = new Feedback();
 			feedback.setId(xmlFeedback.getFeedbackId());
 			feedback.setUserId(xmlFeedback.getUserId());
@@ -67,7 +147,7 @@ public class FeedbackSOAPClient {
 		}
 		return feedbacks;
 	}
-	
+
 	public String submitFeedback(long userId, long eventId, int rating, String body) throws ServiceException_Exception {
 		CreateFeedbackRequest requestParams = factory.createCreateFeedbackRequest();
 		FeedbackData feedback = factory.createFeedbackData();
@@ -76,8 +156,9 @@ public class FeedbackSOAPClient {
 		feedback.setRating(rating);
 		feedback.setBody(body);
 		requestParams.setFeedbackData(feedback);
-		
-		CreateFeedbackResponse response = this.port.createFeedback(requestParams);
+
+		EventManagementPort port = getService(); //load balancing
+		CreateFeedbackResponse response = port.createFeedback(requestParams);
 		return response.getMessage();
 	}
 }
--- soap-proxy-prosumer-rest/src/main/java/it/univaq/sose/dagi/soap_proxy_prosumer_rest/client/TicketSOAPClient.java
+++ soap-proxy-prosumer-rest/src/main/java/it/univaq/sose/dagi/soap_proxy_prosumer_rest/client/TicketSOAPClient.java
@@ -4,11 +4,20 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.xml.datatype.XMLGregorianCalendar;
 
-import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 
 import it.univaq.sose.dagi.soap_proxy_prosumer_rest.Utility;
 import it.univaq.sose.dagi.soap_proxy_prosumer_rest.model.CustomerHistory;
@@ -33,29 +42,97 @@ import it.univaq.sose.dagi.wsdltypes.PurchaseMenuResponse;
 import it.univaq.sose.dagi.wsdltypes.ServiceException_Exception;
 import it.univaq.sose.dagi.wsdltypes.SoldTicketData;
 import it.univaq.sose.dagi.wsdltypes.TicketAvailability;
+import jakarta.ws.rs.ServiceUnavailableException;
+import lombok.extern.slf4j.Slf4j;
 
-@Component
+@Slf4j
+@Service
 public class TicketSOAPClient {
 
 	private ObjectFactory factory;
+	private URL lastUrl;
+	private EurekaClient eurekaClient;
+	private EventManagementImplService service;
+	private final List<InstanceInfo> lastInstancesCache = Collections.synchronizedList(new ArrayList<>());
+	private static final Logger log = LoggerFactory.getLogger(EventSOAPClient.class);
 
-	private EventManagementPort port;
+	private String wsdlUri = "event-management-soap/event-management-soap/?wsdl";
 
-	//@Value("${client.soap.wsdl}")
-		private String wsdlUrl = "http://localhost:8080/api/soap/?wsdl";
-
-	public TicketSOAPClient() {
+	// It initializes the EventSOAPClient with an ObjectFactory instance.
+	// It sets up the necessary configurations to interact with the SOAP service.
+	public TicketSOAPClient(EurekaClient eurekaClient) {
+		// super();
+		this.eurekaClient = eurekaClient;
+		this.service = null;
 		this.factory = new ObjectFactory();
-		URL url;
+	}
+
+	// Returns an instance of the SOAP service among the ones registered to the
+	// discovery server
+	private EventManagementPort getService() {
 		try {
-			url = new URL(wsdlUrl);
+			List<InstanceInfo> instances = Optional
+					.ofNullable(eurekaClient.getInstancesByVipAddress("event-management-soap", false))
+					.filter(list -> !list.isEmpty()).orElseGet(() -> {
+
+						log.warn("Using cached copy of the event management SOAP service");
+						log.warn("lastInstancesCache {}", lastInstancesCache);
+						synchronized (lastInstancesCache) {
+							return new ArrayList<>(lastInstancesCache); // Return a copy of the cached instances
+						}
+					});
+
+			// Update the instance cache synchronously
+			synchronized (lastInstancesCache) {
+				lastInstancesCache.clear();
+				lastInstancesCache.addAll(deepCopyInstanceInfoList(instances));
+			}
+
+			// Remove the last used instance from the list
+			if (lastUrl != null) {
+				instances.removeIf(instance -> {
+					try {
+						return Objects.equals(new URL(instance.getHomePageUrl() + wsdlUri), lastUrl);
+					} catch (MalformedURLException e) {
+						log.error("Malformed URL while filtering instances: {}", e.getMessage(), e);
+						return false;
+					}
+				});
+			}
+
+			// If no alternative instances are available, use the last used instance
+			if (instances.isEmpty()) {
+				log.warn("No alternative instances available for event-management-soap, using the last used instance");
+				if (service != null) {
+					return service.getEventManagementImplPort();
+				} else {
+					throw new ServiceUnavailableException(
+							"event-management-soap: No alternative instances available and no previously used instance available");
+				}
+			}
+
+			// Shuffle the list to select a random instance
+			Collections.shuffle(instances);
+			InstanceInfo instance = instances.get(0);
+			String eurekaUrl = instance.getHomePageUrl() + wsdlUri;
+			URL url = new URL(eurekaUrl);
+			service = new EventManagementImplService(url);
+			log.info("New Retrieved Event Management SOAP URL: {}", url);
+			lastUrl = url;
+
+			return service.getEventManagementImplPort();
 		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return;
+			log.error("Malformed URL: {}", e.getMessage(), e);
+			throw new ServiceUnavailableException("Malformed URL: " + e.getMessage());
+		} catch (Exception e) {
+			log.error("Failed to retrieve Event Management SOAP URL: {}", e.getMessage(), e);
+			throw new ServiceUnavailableException("Failed to retrieve Event Management SOAP URL: " + e.getMessage());
 		}
-		EventManagementImplService service = new EventManagementImplService(url);
-		this.port = service.getEventManagementImplPort();
+	}
+
+	private List<InstanceInfo> deepCopyInstanceInfoList(List<InstanceInfo> instances) {
+		return instances.stream().map(InstanceInfo::new) // Use the copy constructor
+				.collect(Collectors.toList());
 	}
 
 	// This method in the SoldTicketsSOAPClient class retrieves sold ticket details
@@ -70,8 +147,9 @@ public class TicketSOAPClient {
 	// trace, returning null if the process fails.
 	public List<SoldTicket> fetchEventSoldTicketsInfo(Long eventId) throws ServiceException_Exception {
 		FetchEventSoldTicketsRequest requestParams = factory.createFetchEventSoldTicketsRequest();
+		EventManagementPort port = getService(); //load balancing
 		requestParams.setEventId(eventId);
-		FetchEventSoldTicketsResponse response = this.port.fetchEventSoldTickets(requestParams);
+		FetchEventSoldTicketsResponse response = port.fetchEventSoldTickets(requestParams);
 		SoldTicketsList xmlList = response.getSoldTicketsList();
 		List<SoldTicket> soldTicketsInfo = new ArrayList<>();
 		for (SoldTicketData xmlTicket : xmlList.getSoldTicketData()) {
@@ -89,45 +167,46 @@ public class TicketSOAPClient {
 
 	public List<TicketInfo> fetchEventAvailableTickets(long eventId) throws ServiceException_Exception {
 		PurchaseMenuRequest requestParams = factory.createPurchaseMenuRequest();
+		EventManagementPort port = getService(); //load balancing
 		requestParams.setEventId(eventId);
 
-		PurchaseMenuResponse response = this.port.purchaseMenu(requestParams);
+		PurchaseMenuResponse response = port.purchaseMenu(requestParams);
 		// Access the list
 		List<TicketInfo> tickets = new ArrayList<>();
-		for(TicketAvailability xmlTicket : response.getAvailabilitiesList().getTicketAvailability()) {
+		for (TicketAvailability xmlTicket : response.getAvailabilitiesList().getTicketAvailability()) {
 			TicketInfo ticket = new TicketInfo();
 			ticket.setId(xmlTicket.getTicketInfoId());
 			ticket.setEventId(eventId);
 			ticket.setReferenceDate(Utility.toLocalDateTime(xmlTicket.getReferenceDate()));
 			ticket.setAvailableTickets(xmlTicket.getRemainingTickets());
-			
+
 			tickets.add(ticket);
 		}
 		return tickets;
 	}
 
-	public Long purchaseTicket(long eventId, long userId, XMLGregorianCalendar date)
-			throws ServiceException_Exception {
+	public Long purchaseTicket(long eventId, long userId, XMLGregorianCalendar date) throws ServiceException_Exception {
 		CreateSoldTicketRequest requestParams = factory.createCreateSoldTicketRequest();
+		EventManagementPort port = getService(); //load balancing
 		SoldTicketData data = factory.createSoldTicketData();
 		data.setEventId(eventId);
 		data.setUserId(userId);
 		data.setReferenceDate(date);
 		requestParams.setSoldTicketData(data);
 
-		CreateSoldTicketResponse response = this.port.createSoldTicket(requestParams);
+		CreateSoldTicketResponse response = port.createSoldTicket(requestParams);
 
 		return response.getSoldTicketId();
 	}
 
-	public CustomerHistory fetchCustomerTicketHistory(long userId)
-			throws ServiceException_Exception {
+	public CustomerHistory fetchCustomerTicketHistory(long userId) throws ServiceException_Exception {
 		FetchCustomerBoughtTicketsRequest requestParams = factory.createFetchCustomerBoughtTicketsRequest();
+		EventManagementPort port = getService(); //load balancing
 		requestParams.setUserId(userId);
 
-		FetchCustomerBoughtTicketsResponse response = this.port.fetchCustomerBoughtTickets(requestParams);
+		FetchCustomerBoughtTicketsResponse response = port.fetchCustomerBoughtTickets(requestParams);
 
-		//tickets
+		// tickets
 		List<SoldTicket> soldTicketsInfo = new ArrayList<>();
 		for (SoldTicketData xmlTicket : response.getSoldTicketsList().getSoldTicketData()) {
 			SoldTicket ticket = new SoldTicket();
@@ -137,8 +216,8 @@ public class TicketSOAPClient {
 			ticket.setUserId(xmlTicket.getUserId());
 			soldTicketsInfo.add(ticket);
 		}
-		
-		//events
+
+		// events
 		List<Event> ticketEvents = new ArrayList<>();
 		for (EventData xmlEvent : response.getEventsList().getEventData()) {
 			Event eventInfo = new Event();
@@ -151,18 +230,19 @@ public class TicketSOAPClient {
 			eventInfo.setNrTickets(xmlEvent.getNrTickets());
 			ticketEvents.add(eventInfo);
 		}
-		
-		
-		return new CustomerHistory(soldTicketsInfo,ticketEvents);
+
+		return new CustomerHistory(soldTicketsInfo, ticketEvents);
 	}
-	
-	public Long createTicketInfo(long eventId, LocalDateTime referenceDate, int availableTickets) throws ServiceException_Exception {
+
+	public Long createTicketInfo(long eventId, LocalDateTime referenceDate, int availableTickets)
+			throws ServiceException_Exception {
 		CreateTicketInfoRequest requestParams = factory.createCreateTicketInfoRequest();
+		EventManagementPort port = getService(); //load balancing
 		requestParams.setEventId(eventId);
 		requestParams.setReferenceDate(Utility.toXMLCalendar(referenceDate));
 		requestParams.setAvailableTickets(availableTickets);
-		
-		CreateTicketInfoResponse response = this.port.createTicketInfo(requestParams);
+
+		CreateTicketInfoResponse response = port.createTicketInfo(requestParams);
 		return response.getTicketInfoId();
 	}
 }
